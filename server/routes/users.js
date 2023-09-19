b07f8218160054b63972b76c68359b790b97c5de
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-09-19 12:17:53 -0400

    update server

---
+++ server/routes/users.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getUser,
+  getUserAssociates,
+  addRemoveAssociate,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/associates", verifyToken, getUserAssociates);
+
+/* UPDATE */
+router.patch("/:id/:associateId", verifyToken, addRemoveAssociate);
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ import { fileURLToPath } from "url";
 
 import { register } from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 
 import User from "./models/User.js";
 
@@ -50,6 +51,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
